e94758994e5cadc4cbe72b655ec0919d18ebb0c4
Stefan Watt <stefan.watt@gmail.com>
2024-04-10 13:01:58 +0200

    initial

---
+++ keymaps.lua
@@ -0,0 +1,61 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+wezterm.on("tabbed-if-not", function(window, pane)
+	os.execute("bash ~/Scripts/i3-tabbed-if-not.sh")
+end)
+wezterm.on("move_to_scratchpad", function(window, pane)
+	os.execute("i3-msg '[class=\"term-bottom\"]move scratchpad'")
+end)
+
+return {
+	{
+		key = "p",
+		mods = "SUPER",
+		action = act.Multiple({
+			act.EmitEvent("tabbed-if-not"),
+			act.SpawnCommandInNewWindow({
+				label = "Find Project",
+				args = { "zsh", "-ic", "fzf_cd_neovim_tabbed" },
+			}),
+		}),
+	},
+	{
+		key = "Enter",
+		mods = "ALT",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+	{
+		key = "t",
+		mods = "CTRL|SUPER|ALT",
+		action = wezterm.action.SplitPane({
+			direction = "Down",
+			size = { Percent = 20 },
+		}),
+	},
+	{
+		key = "LeftArrow",
+		mods = "SUPER|ALT",
+		action = act.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "RightArrow",
+		mods = "SUPER|ALT",
+		action = act.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "UpArrow",
+		mods = "SUPER|ALT",
+		action = act.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "DownArrow",
+		mods = "SUPER|ALT",
+		action = act.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "h",
+		mods = "SUPER|ALT|SHIFT",
+		action = act.EmitEvent("move_to_scratchpad"),
+	},
+}
---
+++ wezterm.lua
@@ -0,0 +1,31 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "Catppuccin Frappe"
+config.font = wezterm.font("Victor Mono Nerd Font Mono")
+config.font_size = 16.0
+config.font_rules = {
+	{
+		italic = true,
+		font = wezterm.font("VictorMonoNerdFont", { italic = true }),
+	},
+}
+config.warn_about_missing_glyphs = false
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+config.keys = require("keymaps")
+config.window_close_confirmation = "NeverPrompt"
+config.audible_bell = "Disabled"
+return config
